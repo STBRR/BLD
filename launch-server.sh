2d31586ab2784b94b9058070b5d1be74c49a59cd
Liam <liam@gmx.us>
2022-08-17 23:32:16 +0100

    tweak: change password in cli args

--- launch-server.sh
+++ launch-server.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-# The name of the server that'll show in the server browser.
-SERVER_NAME="Deployed by STBRR (Liam#9934)"
-# Set this to blank if you do not want a password on
-SERVER_PASSWORD="changeme"
-
 cd ~/BattalionServerData/Battalion/Binaries/Linux
-./BattalionServer-Linux-Shipping -SteamServerName=$SERVER_NAME -ServerPassword=$SERVER_PASSWORD -Queue="Wartide"
+./BattalionServer-Linux-Shipping -SteamServerName="Deployed by STBRR" -ServerPassword="Password123" -Queue="Wartide"
