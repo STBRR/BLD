6fddd5b29b84b4ac6ce9eebea63b23a829519c56
Liam <liam@gmx.us>
2022-08-17 22:40:41 +0100

    tweak: change path of server binary

--- launch-server.sh
+++ launch-server.sh
@@ -2,9 +2,8 @@
 
 # The name of the server that'll show in the server browser.
 SERVER_NAME="STBRR Battalion Server"
-
 # Set this to blank if you do not want a password on
 SERVER_PASSWORD="changeme"
 
-cd Battalion/Battalion/Binaries/Linux
+cd BattalionServerData/Battalion/Binaries/Linux
 ./BattalionServer-Linux-Shipping -SteamServerName=$SERVER_NAME -ServerPassword=$SERVER_PASSWORD -Queue="Wartide"
