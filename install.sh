a7909546b63b7eadd8fe6c456028d3e8605e6069
Liam <liam@gmx.us>
2022-08-18 01:56:38 +0100

    rework: much cleaner work :+1:

--- install.sh
+++ install.sh
@@ -21,25 +21,14 @@ function banner {
     echo "        Made with <3 by STBRR       "
     echo "             [LIAM#9934]            "
     echo ""
-
 }
 
 function install {
-    # 'steamcmd' is within the non-free repo for debian and multiverse on ubuntu.
-    if grep 'Debian' -q /etc/issue 2>/dev/null; then
-        sudo echo "deb http://deb.debian.org/debian stretch main contrib non-free" >> /etc/aptsources.list
-        sudo echo "deb-src http://deb.debian.org/debian stretch main contrib non-free" >> /etc/apt/sources.list
-
-        sudo echo "deb http://deb.debian.org/debian stretch-updates main contrib non-free" >> /etc/apt/sources.list
-        sudo echo "deb-src http://deb.debian.org/debian stretch-updates main contrib non-free" >> /etc/apt/sources.list
-
-        sudo echo "deb http://security.debian.org/ stretch/updates main contrib non-free" >> /etc/apt/sources.list
-        sudo echo "deb-src http://security.debian.org/ stretch/updates main contrib non-free" >> /etc/apt/sources.list
-        
-    elif grep 'Ubuntu' -q /etc/issue 2>/dev/null; then 
+    if grep 'Debian' -q /etc/issue; then
+        sudo cat src/debian-sources.list >> /etc/apt/sources.list
+    elif grep 'Ubuntu' -q /etc/issue; then 
         sudo add-apt-repository multiverse
     else
-        echo "You're probably running another distro that this script hasn't had in mind.."
         exit
     fi
 
@@ -47,29 +36,20 @@ function install {
     sudo dpkg --add-architecture i386
     sudo apt update -y && sudo apt install -y steamcmd
 
-    clear
-    banner
-    echo "[!] SteamCMD is now installed."
-
-    # now steamcmd is installed, lets install battalion server from it.
     cd ~ && mkdir BattalionServerData && cd BattalionServerData
-
     steamcmd +force_install_dir $(pwd) +login anonymous +app_update 805140 +quit
 
     mkdir ~/.steam/sdk64 && cp steamclient.so ~/.steam/sdk64/
     cp steamclient.so Battalion/Binaries/Linux/
 
-    clear
-    banner
     echo "[*] Battalion Dedicated Server is installed!"
-    echo "[*] You can launch the server with the './launch-server.sh' script."
-    echo "[!] Thank you for using. Reach out on Discord if you need anything."
-
+    echo "[*] You can launch the server using './launch.sh'"
 }
 
-# go ahead with the install
-clear
-banner
-echo "[*] This won't take long.. Confirm any prompts that may popup when installing steamcmd."
-sleep 1
-install
+function main {
+    clear
+    banner
+    install
+}
+
+main
